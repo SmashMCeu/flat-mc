a57c01d636f1873232c6ba48ccb355782ff454f3
Packsolite <packsolite@gmail.com>
2025-04-01 03:40:49 +0200

    :hammer: improve chunk transformer

--- src/main/java/eu/smashmc/flatmc/ChunkTransformer.java
+++ src/main/java/eu/smashmc/flatmc/ChunkTransformer.java
@@ -4,26 +4,21 @@ import eu.smashmc.api.core.Inject;
 import eu.smashmc.api.core.Invoke;
 import eu.smashmc.api.core.Managed;
 import it.unimi.dsi.fastutil.Pair;
-import it.unimi.dsi.fastutil.longs.LongSet;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 
 import java.util.*;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 @Managed
 public class ChunkTransformer implements Listener {
 
 	private static final int MAX_THICKNESS = 2;
-	
+
 	private static final Set<Long> TRANSFORMED = new HashSet<>();
 
 	@Inject
@@ -48,7 +43,7 @@ public class ChunkTransformer implements Listener {
 
 	private void transformChunk(Chunk chunk) {
 		long chunkId = (chunk.getWorld().getUID().getLeastSignificantBits() << 32) + ((long) chunk.getX() << 16) + chunk.getZ();
-		if(!TRANSFORMED.add(chunkId)){
+		if (!TRANSFORMED.add(chunkId)) {
 			return;
 		}
 		for (int x = 0; x < 16; x++) {
@@ -66,17 +61,16 @@ public class ChunkTransformer implements Listener {
 		if (highestBlock.getY() > MAX_THICKNESS) {
 			// find surface materials
 			var topMaterial = getMaterials(chunk, x, highestBlock.getY(), z);
-			
-			if(topMaterial.isEmpty()){
+
+			if (topMaterial.isEmpty()) {
 				return;
 			}
 
 			// erase terrain
-			for (int y = highestBlock.getY(); y >= topMaterial.size(); y--) {
+			for (int y = Math.max(highestBlock.getY(), 64); y >= topMaterial.size(); y--) {
 				var block = chunk.getBlock(x, y, z);
-				if(!block.getType().isAir()) {
-					block.setType(Material.AIR, false, false);
-				}
+				if (block.getType() != Material.AIR)
+					block.setType(Material.AIR, false, true);
 			}
 
 			// place flat surface
@@ -84,7 +78,7 @@ public class ChunkTransformer implements Listener {
 			int y = 0;
 			for (var top : topMaterial) {
 				var block = chunk.getBlock(x, y, z);
-				if(block.getType() != top.left())
+				if (block.getType() != top.left())
 					block.setTypeIdAndData(top.left().getId(), top.right(), false);
 				y++;
 			}
@@ -94,17 +88,14 @@ public class ChunkTransformer implements Listener {
 	private List<Pair<Material, Byte>> getMaterials(Chunk chunk, int x, int y, int z) {
 		List<Pair<Material, Byte>> materials = new ArrayList<>();
 		var lastmat = Material.AIR;
-		int solids = 0;
-		while (solids < MAX_THICKNESS && y >= 0) {
+		while (materials.size() < MAX_THICKNESS && y >= 0) {
 			var block = chunk.getBlock(x, y, z);
 			var mat = block.getType();
 			var data = block.getData();
-			if ((!mat.isAir() && solids == 0 || mat.isSolid()) && mat != lastmat && !mat.name().startsWith("LEAVES")) {
+			boolean isKept = mat.isSolid() && mat.isOccluding() && !mat.name().startsWith("LEAVES") || mat.name().contains("WATER");
+			if (isKept && mat != lastmat) {
 				materials.add(Pair.of(mat, data));
 				lastmat = mat;
-				if ((!mat.isTransparent() || mat.name().contains("WATER")) ) {
-					solids++;
-				}
 			}
 			y--;
 		}
